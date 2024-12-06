fa2b652c795ab77ea52e197cb225b50db89a75a6
Omar <omarfessy@gmail.com>
2024-12-06 15:09:27 +0100

    keep the core functionality separate from the CLI layer

--- servier/main.py
+++ servier/main.py
@@ -86,47 +86,9 @@ def cross_reference_models(
     return cross_reference, cross_reference_errors
 
 
-@click.group()
-def cli():
-    pass
-
-
-@click.command()
-@click.option(
-    "--raw-pubclinical-data",
-    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
-    default=PUBLICATIONS,
-    show_default=True,
-    help="Path to the raw pubclinical data.",
-)
-@click.option(
-    "--raw-drug-data",
-    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
-    default=DRUGS,
-    show_default=True,
-    help="Path to the raw drug data.",
-)
-@click.option(
-    "--silver_zone_path",
-    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
-    default=SILVER_ZONE,
-    show_default=True,
-    help="Path to the silver zone.",
-)
-@click.option(
-    "--trash_zone_path",
-    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
-    default=CORRUPTED_DATA_ZONE,
-    show_default=True,
-    help="Path to the trash zone for corrupted data.",
-)
-def main_pipeline(
+def _main_pipeline(
     raw_pubclinical_data, raw_drug_data, silver_zone_path, trash_zone_path
 ) -> None:
-    """Main pipeline to process data."""
-    click.echo(f"Processing pubclinical data from {raw_pubclinical_data}")
-    click.echo(f"Processing drug data from {raw_drug_data}")
-    click.echo(f"Storing results in {silver_zone_path}")
 
     pubtrials_data_files = list_files_in_folder(
         raw_pubclinical_data, PUBTRIALS_FILE_NAMES
@@ -173,22 +135,7 @@ def main_pipeline(
         del errors
 
 
-@click.command()
-@click.option(
-    "--silver-zone-path",
-    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
-    default=SILVER_ZONE,
-    show_default=True,
-    help="Path to the silver zone.",
-)
-@click.option(
-    "--gold-zone-path",
-    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
-    default=GOLD_ZONE,
-    show_default=True,
-    help="Path to the gold zone.",
-)
-def journal_with_max_drugs(
+def _journal_with_max_drugs(
     silver_zone_path: pathlib.Path, gold_zone_path: pathlib.Path
 ) -> None:
     try:
@@ -209,23 +156,7 @@ def journal_with_max_drugs(
         save_file_as_json(gold_zone_path / f"the_journal_{now}.json", the_journal)
 
 
-@click.command()
-@click.argument("drug_name", type=str)
-@click.option(
-    "--silver-zone-path",
-    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
-    default=SILVER_ZONE,
-    show_default=True,
-    help="Path to the silver zone.",
-)
-@click.option(
-    "--gold-zone-path",
-    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
-    default=GOLD_ZONE,
-    show_default=True,
-    help="Path to the gold zone.",
-)
-def get_drugs_from_journals_that_mention_a_specific_drug(
+def _get_drugs_from_journals_that_mention_a_specific_drug(
     silver_zone_path: pathlib.Path, gold_zone_path: pathlib.Path, drug_name: str
 ) -> None:
     try:
@@ -249,7 +180,99 @@ def get_drugs_from_journals_that_mention_a_specific_drug(
     )
     save_file_as_json(
         gold_zone_path / f"drugs_by_journals_by_{drug_name}_{now}.json",
-        drugs_by_journals,
+        list(drugs_by_journals),
+    )
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option(
+    "--raw-pubclinical-data",
+    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
+    default=PUBLICATIONS,
+    show_default=True,
+    help="Path to the raw pubclinical data.",
+)
+@click.option(
+    "--raw-drug-data",
+    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
+    default=DRUGS,
+    show_default=True,
+    help="Path to the raw drug data.",
+)
+@click.option(
+    "--silver_zone_path",
+    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
+    default=SILVER_ZONE,
+    show_default=True,
+    help="Path to the silver zone.",
+)
+@click.option(
+    "--trash_zone_path",
+    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
+    default=CORRUPTED_DATA_ZONE,
+    show_default=True,
+    help="Path to the trash zone for corrupted data.",
+)
+def main_pipeline(
+    raw_pubclinical_data, raw_drug_data, silver_zone_path, trash_zone_path
+) -> None:
+    """Main pipeline to process data."""
+    click.echo(f"Processing pubclinical data from {raw_pubclinical_data}")
+    click.echo(f"Processing drug data from {raw_drug_data}")
+    click.echo(f"Storing results in {silver_zone_path}")
+
+    _main_pipeline(
+        raw_pubclinical_data, raw_drug_data, silver_zone_path, trash_zone_path
+    )
+
+
+@click.command()
+@click.option(
+    "--silver-zone-path",
+    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
+    default=SILVER_ZONE,
+    show_default=True,
+    help="Path to the silver zone.",
+)
+@click.option(
+    "--gold-zone-path",
+    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
+    default=GOLD_ZONE,
+    show_default=True,
+    help="Path to the gold zone.",
+)
+def journal_with_max_drugs(
+    silver_zone_path: pathlib.Path, gold_zone_path: pathlib.Path
+) -> None:
+    _journal_with_max_drugs(silver_zone_path, gold_zone_path)
+
+
+@click.command()
+@click.argument("drug_name", type=str)
+@click.option(
+    "--silver-zone-path",
+    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
+    default=SILVER_ZONE,
+    show_default=True,
+    help="Path to the silver zone.",
+)
+@click.option(
+    "--gold-zone-path",
+    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
+    default=GOLD_ZONE,
+    show_default=True,
+    help="Path to the gold zone.",
+)
+def get_drugs_from_journals_that_mention_a_specific_drug(
+    silver_zone_path: pathlib.Path, gold_zone_path: pathlib.Path, drug_name: str
+) -> None:
+    _get_drugs_from_journals_that_mention_a_specific_drug(
+        silver_zone_path, gold_zone_path, drug_name
     )
 
 
